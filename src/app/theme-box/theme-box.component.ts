df9bc4b46823c680f002e0ee624ac98d0b0f9820
John Campbell Hinrichs <jchinrichs7@gmail.com>
2022-05-06 18:55:40 -0500

    added an API for word association

--- src/app/theme-box/theme-box.component.ts
+++ src/app/theme-box/theme-box.component.ts
@@ -14,16 +14,37 @@ export class ThemeBoxComponent implements OnInit {
     letters: []
   }
 
+  association: string[] = ['hi', 'there']
+
   //this is if the user hits submit
   submitTheme(): void {
     this.getValues((<HTMLInputElement>document.getElementById("text")).value.toString())
+  }
+
+  copyValues(arr:any): void{
+    this.association = arr.associations.replace(/,/g, '').split(" ");
+    console.log(this.association)
+    
   }
 
   //this is if the user hits enter on the box
   getValues(val: string)
   {
     this.theme.value = val;
+    const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'twinword-word-associations-v1.p.rapidapi.com',
+      'X-RapidAPI-Key': '82c7604abfmsh1a50896f4be8971p1886cejsn0203aeca9f75'
+    }
+  };
+  fetch('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry=sound', options)
+    .then(response => response.json())
+    .then(response => this.copyValues(response))
+    .catch(err => console.error(err));
   }
+
+
 
   constructor() { }
 
@@ -31,3 +52,4 @@ export class ThemeBoxComponent implements OnInit {
   }
 
 }
+
