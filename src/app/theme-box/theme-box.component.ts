84f967ff173459efac368600134f879055cce1bc
John Campbell Hinrichs <jchinrichs7@gmail.com>
2022-05-08 18:48:39 -0500

    A slightly better version

--- src/app/theme-box/theme-box.component.ts
+++ src/app/theme-box/theme-box.component.ts
@@ -1,3 +1,4 @@
+//https://www.freecodecamp.org/news/async-await-javascript-tutorial/
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Theme } from '../theme';
@@ -14,22 +15,49 @@ export class ThemeBoxComponent implements OnInit {
     value: "",
     letters: []
   }
-
+  letters: boolean[] = []
   association: string[] = []
+
+  setError(err:any): void {
+    console.error(err);
+    this.errorMsg = "Try another word."
+  }
 
   //this is if the user hits submit
   submitThemeButton(): void {
     this.submitThemeEnterKey((<HTMLInputElement>document.getElementById("text")).value.toString())
   }
 
-  setError(err:any): void {
-    console.error(err);
-    this.errorMsg = "Try another word."
+  //this is if the user hits enter on the box
+  submitThemeEnterKey(val: string)
+  {
 
-  }
-  copyValues(arr: any): void{
-    this.association = arr.associations.replace(/,/g, '').split(" ");
-    console.log(this.association)
+    //set the theme value
+    this.theme.value = val;
+
+    //set [a=false; b=false .... z=false]
+    for(let i = 0; i < 26; i++)
+    {
+      this.letters.push(false)
+    }
+
+    //for the theme word, set values to true. letters[0] = a
+    for(let i = 0; i < val.length; i++)
+    {
+      this.letters[Number(val.toLowerCase().charCodeAt(i) - 96)-1] = true
+    }
+
+    console.log(this.letters)
+
+    this.getAssociatedWords(this.theme.value)
+
+    this.getDiccionaryInformation(this.theme.value)
+
+    console.log(this.association.length + "this is length");
+    for(let i = 0; i < this.association.length; i++)
+    {
+      this.getDiccionaryInformation(this.association[i]);
+    }
   }
 
   getAssociatedWords(word: string)
@@ -41,32 +69,43 @@ export class ThemeBoxComponent implements OnInit {
       'X-RapidAPI-Key': '82c7604abfmsh1a50896f4be8971p1886cejsn0203aeca9f75'
     }
   };
+
+  /*
+    this returns an array of length 1 with a string of words separated by comma  
+  */
   fetch('https://twinword-word-associations-v1.p.rapidapi.com/associations/?entry='+word, options)
     .then(response => response.json())
-    .then(response => this.copyValues(response))
+    .then(response => {this.copyValues(response)})
     .then( () => this.errorMsg = '')
     .catch(err => this.setError(err)); 
   }
 
-  getPartsOfSpeech(arr:any)
+  getDiccionaryInformation(word: string)
   {
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
+    .then(response => response.json())
+    .then(response => { this.getPartsOfSpeech(word, response[0].meanings) })
+  }
+
+
+  copyValues(arr: any): void{
+    this.association = arr.associations.replace(/,/g, '').split(" ");
+    console.log(this.association)
+  }
+
+  getPartsOfSpeech(word: string, arr:any)
+  {
+    let noun = false
+    let verb = false
+    let adjective = false
+    let adverb = false
+   
     for (let i = 0; i < arr.length; i++)
     {
-      console.log(this.theme.value + ": " + arr[i].partOfSpeech)
+      console.log(word + ": " + arr[i].partOfSpeech)
     }
   }
 
-  //this is if the user hits enter on the box
-  submitThemeEnterKey(val: string)
-  {
-    this.theme.value = val;
-    this.getAssociatedWords(this.theme.value)
-
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+this.theme.value.toString())
-    .then(response => response.json())
-    .then(response => this.getPartsOfSpeech(response[0].meanings))
-  
-  }
 
   constructor() { }
 
